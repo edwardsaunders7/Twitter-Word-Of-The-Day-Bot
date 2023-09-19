1821a7e3856160d3f0e7d6f7d982286935da33ce
Edward Saunders <51308267+edwardsaunders7@users.noreply.github.com>
2023-09-19 13:02:43 +0100

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -4,11 +4,11 @@ from PyDictionary import PyDictionary
 import renameGifs
 
 
-consumer_key = "nDZ9rWxijlxGoYIly1ZA0heT0"
-consumer_secret_key = "Yv7OjoX20T2Fi5N6xbffZcK7U9IoB18kqphTWFUACfufJXgiUX"
+consumer_key = ""
+consumer_secret_key = ""
 
-access_token = "1292602359076605953-viJYPtgMqiH4Lqv8H8i8LgM7lN1pVM"
-access_token_secret = "PCaE2bmD41NyOkScxBdFZggznR05iOwuUnFr08uGBA5UR"
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token,access_token_secret)
